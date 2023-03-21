ec27361f9e0aed456d218f2ab264d3766fe836e3
CHARLOTTE TUTON <C.TUTON-2021@hull.ac.uk>
2023-03-21 14:10:06 +0000

    calendar

---
+++ BlazorApp1/Data/CalanderEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Data
+{
+    public class CalanderEvent
+    {
+        public int EventId { get; set; }
+        public string Note { get; set; }
+        public DateTime EventDate { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);      
+        public string DateValue { get; set; }
+        public string DayName { get; set; }
+        public string Message { get; set; }
+
+    }
+}
---
+++ BlazorApp1/Data/WeekClass.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp1.Data
+{
+    public class WeekClass
+    {
+        public List<CalanderEvent> Dates { get; set; }= new List<CalanderEvent>(); 
+    }
+}
